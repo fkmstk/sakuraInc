ddccc428c26bc59cb2b82a67303cf350e648e948
fkmstk <fkmstk@gmail.com>
2026-02-16 20:36:23 +0900

    Initial Commit

---
+++ sakuraInc/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  sakuraInc
+//
+//  Created by 福井　正剛 on 2026/02/16.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
---
+++ sakuraIncTests/sakuraIncTests.swift
@@ -0,0 +1,17 @@
+//
+//  sakuraIncTests.swift
+//  sakuraIncTests
+//
+//  Created by 福井　正剛 on 2026/02/16.
+//
+
+import Testing
+@testable import sakuraInc
+
+struct sakuraIncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
